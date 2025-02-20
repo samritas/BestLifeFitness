39b011422201cc147db9d9a269f52e348161a7af
unknown <samrawittesfahun19@gmail.com>
2025-02-19 21:37:53 -0800

    some changes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2', // Customize primary color
+      main: '#1976d2', 
     },
     secondary: {
-      main: '#dc004e', // Customize secondary color
+      main: '#dc004e', 
     },
   },
 });
